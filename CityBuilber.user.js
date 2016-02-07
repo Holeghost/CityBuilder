abb4d3e2084c803e78c33f06c62c9cb7c53368a5
Holeghost <mission_007_christian@hotmail.com>
2016-02-08 00:36:44 +0100

    Update CityBuilber.user.js

--- CityBuilber.user.js
+++ CityBuilber.user.js
@@ -59,7 +59,8 @@ function waitForClientLib() {
 	  // }
 	  // return rowData;
 	// }
-    // Add a TabView to window
+    
+	// Add a TabView to window
     var tabView = new qx.ui.tabview.TabView;
     win.add(tabView, {
         flex: 1
@@ -70,67 +71,188 @@ function waitForClientLib() {
         'https://eaassets-a.akamaihd.net/cncalliancesgame/cdn/data/98abd73f92a4fb8f5f3a28a1b2a82344.png'
     );
 	page1.setLayout(new qx.ui.layout.HBox());
-	tabView.add(page1);
+	tabView.add(page1);	
+	//Silo button
+    var tSiloBtn = new qx.ui.form.Button('Silo');
+    // Add button to position on screen
+    page1.add(tSiloBtn, {
+        right: 3,
+        top: 3
+    });
+	//Harvest button
+	var tHarvBtn = new qx.ui.form.Button('Harvest');
+    // Add button to position on screen
+    page1.add(tHarvBtn, {
+        right: 3,
+        top: 3
+    });
+	//Select bases
+	var tSelectBtn = new qx.ui.form.Button('Select All');
+    // Add button to position on screen
+    page1.add(tSelectBtn, {
+        right: 3,
+        top: 3
+    });
+	
+	//if tables could be added to all pages except options  >>>>>>>>>
 	//Tib table
 	
-	//var tableA = new qx.ui.table.Table(tableModelA)
-		//.set({
+	// var tableA = new qx.ui.table.Table(tableModelA)
+		// .set({
 			// decorator: null,
-			 // Padding: 3,
 			// height: 200,
-			 // width: 300
+			// bottom: 0
 		// });
 	
 	// tableModelA = new qx.ui.table.model.Simple();
 	// tableModelA.setColumns([
 		// "baseName",
 		// "Boolean",
-		// "Status"
+		// "Status" 
 	// ]);
 	// tableModelA.setData(createRandomRows(1000));
 	// page1.add(tableA);
+		
+		//<<<<<<<<<<<
 		
 	//////////All About CryPage///////////
     var page2 = new qx.ui.tabview.Page('Cry',
         'https://eaassets-a.akamaihd.net/cncalliancesgame/cdn/data/61f096dde442bd3be1843a0929900194.png'
     );
-	page2.setLayout(new qx.ui.layout.VBox());
+	page2.setLayout(new qx.ui.layout.HBox());
     tabView.add(page2);
+	//Silo button
+	var cSiloBtn = new qx.ui.form.Button('Silo');
+    // Add button to position on screen
+    page2.add(cSiloBtn, {
+        right: 3,
+        top: 3
+    });
+	//Harvest button
+	var cHarvBtn = new qx.ui.form.Button('Harvest');
+    // Add button to position on screen
+    page2.add(cHarvBtn, {
+        right: 3,
+        top: 3
+    });
+	//Select bases
+	var cSelectBtn = new qx.ui.form.Button('Select All');
+    // Add button to position on screen
+    page2.add(cSelectBtn, {
+        right: 3,
+        top: 3
+    });
 	
 	///////////All About PowPage//////////
     var page3 = new qx.ui.tabview.Page('Pow',
         'https://eaassets-a.akamaihd.net/cncalliancesgame/cdn/data/fa6798783e2c662ce81e861990aef03a.png'
     );
-	page3.setLayout(new qx.ui.layout.VBox());
+	page3.setLayout(new qx.ui.layout.HBox());
     tabView.add(page3);
+	
+	//PowPlant button
+	var ppBtn = new qx.ui.form.Button('PP');
+    // Add button to position on screen
+    page3.add(ppBtn, {
+        right: 3,
+        top: 3
+    });	
+	//Acc button
+	var AccBtn = new qx.ui.form.Button('Acc');
+    // Add button to position on screen
+    page3.add(AccBtn, {
+        right: 3,
+        top: 3
+    });	
+	// select bases
+	var pSelectBtn = new qx.ui.form.Button('Select All');
+    // Add button to position on screen
+    page3.add(pSelectBtn, {
+        right: 3,
+        top: 3
+    });
 	
 	///////////All About CashPage///////////
     var page4 = new qx.ui.tabview.Page('Cash',
         'https://eaassets-a.akamaihd.net/cncalliancesgame/cdn/data/a7d2f83e4fe41fc03990192217fd0330.png'
     );
-	page4.setLayout(new qx.ui.layout.VBox());
+	page4.setLayout(new qx.ui.layout.HBox());
     tabView.add(page4);
+	//Refinery button
+	var refBtn = new qx.ui.form.Button('Ref');
+    // Add button to position on screen
+    page4.add(refBtn, {
+        right: 3,
+        top: 3
+    });
+	//Select button
+	var rSelectBtn = new qx.ui.form.Button('Select All');
+    // Add button to position on screen
+    page4.add(rSelectBtn, {
+        right: 3,
+        top: 3
+    });
 	
 	///////////All About BasePage///////////
     var page5 = new qx.ui.tabview.Page('Base',
         'https://eaassets-a.akamaihd.net/cncalliancesgame/cdn/data/70271aea3ca93c19f84c3fbe7645b76d.png'
     );
-	page5.setLayout(new qx.ui.layout.VBox());
+	page5.setLayout(new qx.ui.layout.HBox());
     tabView.add(page5);
+	//Defence buildings button
+	var bDefBtn = new qx.ui.form.Button('Defence');
+    // Add button to position on screen
+    page5.add(bDefBtn, {
+        right: 3,
+        top: 3
+    });
+    
+    //the rest doesn't show from here on
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Offence buildings button
+	var bOffbBtn = new qx.ui.form.Button('Offence');
+    // Add button to position on screen
+    page5.add(bOffBtn, {
+        right: 3,
+        top: 3
+    });
+	//Select button
+	var bSelectBtn = new qx.ui.form.Button('Select All');
+    // Add button to position on screen
+    page5.add(bSelectBtn, {
+        right: 3,
+        top: 3
+    });
 	
 	///////////All About DefPage///////////
     var page6 = new qx.ui.tabview.Page('Def',
         'https://eaassets-a.akamaihd.net/cncalliancesgame/cdn/data/17dab9ef93b23f2156ff6c0f235ccfd7.png'
     );
-	page6.setLayout(new qx.ui.layout.VBox());
+	page6.setLayout(new qx.ui.layout.HBox());
     tabView.add(page6);
+	//Select button
+	var DefSelectBtn = new qx.ui.form.Button('Select All');
+    // Add button to position on screen
+    page6.add(DefSelectBtn, {
+        right: 3,
+        top: 3
+    });
 	
 	///////////All About OffPage///////////
     var page7 = new qx.ui.tabview.Page('Off',
         'https://eaassets-a.akamaihd.net/cncalliancesgame/cdn/data/c4c2f864fc9ca46d3b1ff48b5a7bc3d2.png'
     );
-	page7.setLayout(new qx.ui.layout.VBox());
+	page7.setLayout(new qx.ui.layout.HBox());
     tabView.add(page7);
+	//Select button
+	var OffSelectBtn = new qx.ui.form.Button('Select All');
+    // Add button to position on screen
+    page7.add(OffSelectBtn, {
+        right: 3,
+        top: 3
+    });
 	
 	///////////All About OptionPage///////////
     var page8 = new qx.ui.tabview.Page('Options',
